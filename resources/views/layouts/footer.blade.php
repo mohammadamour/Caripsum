2f1ea179a7c52610ed699cf435768001afb35be1
somet <mohammadalamoor96@gmail.com>
2026-09-01 12:32:31 +0300

    fixed footer alignment and added link to my personal site

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,15 @@
 <footer style="background-color: var(--primary-color); padding: 2rem 0; margin-top: auto;">
     <div class="container flex justify-between items-center">
-        <!-- Left Side: Coded by -->
         <div style="color: white; font-weight: 500;">
-            Coded by 
-            <!-- <a href="" target="_blank" style="color: white; text-decoration: underline;"> -->
+            Coded by
+            <a
+                href="https://mohammadamour.github.io/Mohammad-Altayeb/"
+                target="_blank"
+                rel="noreferrer"
+                style="color: white; text-decoration: underline;"
+            >
                 Mohammad Alamour
-            <!-- </a> -->
+            </a>
         </div>
-
     </div>
 </footer>
